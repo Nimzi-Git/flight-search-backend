c4ffed72c4e9ad18db5f59c7e888fc090d3a79e2
C0206217 <nirman.mandal@isbanuk.com>
2018-04-05 10:40:43 +0100

    Updated CORS filter

--- src/main/java/com/nirman/rest/webservices/flightsearchwebservices/FlightsearchWebServicesApplication.java
+++ src/main/java/com/nirman/rest/webservices/flightsearchwebservices/FlightsearchWebServicesApplication.java
@@ -2,11 +2,32 @@ package com.nirman.rest.webservices.flightsearchwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FlightsearchWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsearchWebServicesApplication.class, args);
+	}
+	@ConditionalOnProperty(name = "cors.enabled", havingValue = "true", matchIfMissing = true)
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "PUT", "POST")
+						.allowCredentials(true);
+
+			}
+		};
 	}
 }
